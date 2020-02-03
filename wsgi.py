8c85ebac028e3cc61a1ee83e8c53217a5a0e7b33
Ruffin Arnaud <arnaud.ruffin@orange.com>
2020-02-03 20:43:21 +0100

    handle proxy request

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from requests import get
+import os
+
 application = Flask(__name__)
+
+TARGET="https://plazza.orange.com/api/"
 
 @application.route("/")
 def hello():
     return "Hello You!"
+
+@application.route("/api/<path: path>", methods=["GET"])
+def proxy(path):
+    redirection = "%s%s" % (TARGET,path)
+    println(redirection)
+    user = os.environ['USER']
+    password = os.envion["PASS"]
+    return get(redirection,auth=(user,password)).content
 
 if __name__ == "__main__":
     application.run()
