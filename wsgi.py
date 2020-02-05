8ccc37dc0362d246d78fea3e6b3e54813d930f00
Ruffin Arnaud <arnaud.ruffin@orange.com>
2020-02-05 18:01:06 +0100

    🐤

--- wsgi.py
+++ wsgi.py
@@ -14,8 +14,8 @@ def hello():
 def hande_preflight(path):
     return "You rule, preflight!"
 
-@application.route("/api/<path:path>", methods=["GET"])
-def proxy(path):
+@application.route("/old/<path:path>", methods=["GET"])
+def old_school_proxy(path):
     url = "%s%s?%s" % (TARGET,path,request.query_string.decode("utf-8"))
     print(url)
     print(request.query_string)
@@ -29,6 +29,7 @@ def proxy(path):
     }
     req = requests.get(url, auth=(user, password), headers=headers)
     if((req.status_code == requests.codes.ok) or (req.status_code == 201)):
+        req.headers['Access-Control-Allow-Origin'] = '*'
         return req.content
     else:
         print("Error: ")
@@ -40,6 +41,34 @@ def proxy(path):
         #abort(Response(req.text))
         return req
         #return "Error"
+
+
+method_requests_mapping = {
+    'GET': requests.get,
+    'HEAD': requests.head,
+}
+
+@application.route("/api/<path:path>", methods=method_requests_mapping.keys())
+def proxy(path):
+    url = "%s%s?%s" % (TARGET,path,request.query_string.decode("utf-8"))
+    print(url)
+    print(request.query_string)
+    user = os.environ['USER']
+    password = os.environ["PASS"]
+    headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    'Postman-Token': "cecfaf76-0b4b-4e37-8fb6-d1e4b01a0288",
+    'User-Agent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+    }
+
+    requests_function = method_requests_mapping[flask.request.method]
+    request = requests_function(url, auth=(user, password), headers=headers, stream=True, params=flask.request.args)
+    response = flask.Response(flask.stream_with_context(request.iter_content()),
+                              content_type=request.headers['content-type'],
+                              status=request.status_code)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == "__main__":
     application.run()
